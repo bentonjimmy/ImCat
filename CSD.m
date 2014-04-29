098e08fa02a276e819af4fe00ee6cb26af063ed8
bentonjimmy <benton.jimmy@gmail.com>
2014-04-28 22:08:31 -0400

    Tested CSD
    Tested CSD

--- CSD.m
+++ CSD.m
@@ -1,9 +1,12 @@
 function [descriptor] = CSD(im)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+%CSD Creates the Color Structure Descriptor
+%   Using the given image im, this function returns the Color Structure
+%   Descriptor of the image.  The descriptor is found by first converting
+%   the RGB image to an HMMD image, where H is hue, M is the max RGB value,
+%   M is the min RGB value and D is the difference between the max and min.
 
 hsvim = rgb2hsv(im); %convert to hsv image
-[r, c] = size(im); %get the size of the input image
+[r, c, ~] = size(im); %get the size of the input image
 
 %create the empty HMMD images, it only needs to have values for Hue, Diff
 %and Sum
@@ -11,16 +14,18 @@ HMMDim = zeros(r, c, 3);
 %convert image to HMMD image
 for i=1:r
    for j=1:c
-       maxrgb = max(im(i, j, :));
-       minrgb = min(im(i, j, :));
-       HMMDim(i, j, 1) = hsvim(i, j, 1) * 360; %convert hue to 360 degrees
-       HMMDim(i, j, 2) = maxrgb - minrgb; %calculate diff
-       HMMDim(i, j, 3) = (maxrgb + minrgb)/2; %calculate sum
+       maxrgb = uint16(max(im(i, j, :)));
+       minrgb = uint16(min(im(i, j, :)));
+       angle = uint16(hsvim(i, j, 1) * 360);
+       HMMDim(i, j, 1) = angle; %convert hue to 360 degrees
+       difference = maxrgb - minrgb;
+       HMMDim(i, j, 2) = difference; %calculate diff
+       avgsum = (maxrgb + minrgb) / 2;
+       HMMDim(i, j, 3) = avgsum; %calculate sum
    end
 end
 
-%descriptor = zeros(1, 255); %the final image descriptor
-%THE QUANTIZATION IS NOT LINEAR; HOWS SHOULD IT BE STORED?
+descriptor = zeros(1, 255); %the final image descriptor
 
 %evaluate each pixel in the image
 for i=1:(r-7)
@@ -34,28 +39,35 @@ for i=1:(r-7)
              diff = HMMDim(a, b, 2);
              sum = HMMDim(a, b, 3);
              z=1;
-             while(z<=64)
+             while(z<=64)%Check if the value already appears in vector or needs to be added
                 if((structure_values(z, 1) == hue && structure_values(z, 2) == diff && structure_values(z, 3) == sum) || (structure_values(z, 1) == 0 && structure_values(z, 2) == 0 && structure_values(z, 3) == 0))
                     swhist(z) = swhist(z)+1;
+                    structure_values(z, 1) = hue;
+                    structure_values(z, 2) = diff;
+                    structure_values(z, 3) = sum;
+                    z = 65; %exit the loop
                 end
                 z = z+1;
              end
           end
        end
        y=1;
+       %Add the colors found in the 8x8 structure to the CSD histogram
        while((y<=64) && (swhist(y) ~= 0))
            %quantize the values in the sw histogram 
-           if(diff < 6)
-               sumval = floor((structure_values(y, 3)*255) / 8);
-           elseif(diff < 20)
-                   
-           elseif(diff < 60)
-               
-           elseif(diff < 110)
-               
+           if(structure_values(y, 2) < 6)
+               val = floor(structure_values(y, 3) / 8);
+           elseif(structure_values(y, 2) < 20)
+               val = 33 + (floor(structure_values(y, 1)/90)*8) + floor(structure_values(y, 3)/32);
+           elseif(structure_values(y, 2) < 60)
+               val = 65 + (floor(structure_values(y, 1)/22.5)*4) + floor(structure_values(y, 3)/64);
+           elseif(structure_values(y, 2) < 110)
+               val = 129 + (floor(structure_values(y, 1)/22.5)*4) + floor(structure_values(y, 3)/64);
            else
-               
+               val = 193 + (floor(structure_values(y, 1)/22.5)*4) + floor(structure_values(y, 3)/64);
            end
+           %Increment the histogram by 1
+           descriptor(val) = descriptor(val) + 1;
            y = y+1;
        end
    end
